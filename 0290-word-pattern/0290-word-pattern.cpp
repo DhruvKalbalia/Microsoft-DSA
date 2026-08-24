bf448452c9951f25c0ef51500051ff59752a17bc
DhruvKalbalia <64431395+DhruvKalbalia@users.noreply.github.com>
2026-08-24 19:34:18 +0530

    Time: 0 ms (100%), Space: 8.8 MB (41.17%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string> mp1;
+        unordered_map<string, char> mp2;
+
+        stringstream ss(s);//splits string to individual words
+        string word;
+
+        int i=0;
+        while(ss>>word){
+            if (i==pattern.size())
+                return false;
+
+            char ch = pattern[i];
+
+            if (mp1.find(ch)!=mp1.end() && mp1[ch] != word)
+                return false;
+
+            if (mp2.find(word) != mp2.end() && mp2[word] != ch)
+                return false;
+
+            mp1[ch] = word;
+            mp2[word] = ch;
+
+            i++;
+        }
+
+        return i==pattern.size();
+    }
+};
