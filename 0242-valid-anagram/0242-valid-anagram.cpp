bcdf41edb2a490bf9b78c93533bb3e6b8ad2c6ed
DhruvKalbalia <64431395+DhruvKalbalia@users.noreply.github.com>
2026-08-24 19:40:03 +0530

    Time: 1 ms (54.11%), Space: 9.8 MB (49.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size()==t.size()){
-            sort(s.begin(),s.end());
-            sort(t.begin(),t.end());
-            if (s==t)
-                return true;
+        if (s.size()!=t.size())
+            return false;
+
+        unordered_map<char,int> mp;
+        for (char c:s)
+        mp[c]++;
+
+        for (char c:t){
+            mp[c]--;
+
+            if (mp[c]<0)
+                return false;
         }
-        return false;
+        return true;
     }
 };
